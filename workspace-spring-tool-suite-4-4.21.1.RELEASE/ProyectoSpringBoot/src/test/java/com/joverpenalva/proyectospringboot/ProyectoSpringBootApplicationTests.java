c854770f787c02b6d937a31326fc921e20cc0324
Sergipks <117666099+Sergipks@users.noreply.github.com>
2024-05-09 17:07:10 +0200

    Add files via upload

---
+++ workspace-spring-tool-suite-4-4.21.1.RELEASE/ProyectoSpringBoot/src/test/java/com/joverpenalva/proyectospringboot/ProyectoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joverpenalva.proyectospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
