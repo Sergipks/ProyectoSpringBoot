b6fcd9c889933c26b558b1444e9cb7ecf69bea11
Sergio Jover <jover.std@gmail.com>
2024-05-15 18:35:53 +0200

    Funcionalidades parte Sergio

--- src/main/java/com/joverpenalva/proyectospringboot/models/dao/ITrabajoDAO.java
+++ src/main/java/com/joverpenalva/proyectospringboot/models/dao/ITrabajoDAO.java
@@ -1,9 +1,18 @@
 package com.joverpenalva.proyectospringboot.models.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.joverpenalva.proyectospringboot.models.entities.Trabajo;
 
 public interface ITrabajoDAO extends CrudRepository<Trabajo, String> {
+	
+	@Query("SELECT t FROM Trabajo t WHERE t.idTrabajador = :idTrabajador AND t.contraseña = :contraseña AND t.fechaFin IS NULL")
+    List<Trabajo> findPendientesTrabajador(String idTrabajador, String contraseña);
+	
+	@Query("SELECT t FROM Trabajo t WHERE t.idTrabajador = :idTrabajador AND t.contraseña = :contraseña AND t.fechaFin IS NOT NULL")
+    List<Trabajo> findFinalizadosTrabajador(String idTrabajador, String contraseña);
 
 }
--- src/main/java/com/joverpenalva/proyectospringboot/models/services/TrabajoServiceImpl.java
+++ src/main/java/com/joverpenalva/proyectospringboot/models/services/TrabajoServiceImpl.java
@@ -1,10 +1,13 @@
 package com.joverpenalva.proyectospringboot.models.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.joverpenalva.proyectospringboot.models.entities.Trabajador;
+import com.joverpenalva.proyectospringboot.models.dao.ITrabajadorDAO;
 import com.joverpenalva.proyectospringboot.models.dao.ITrabajoDAO;
 import com.joverpenalva.proyectospringboot.models.entities.Trabajo;
 
@@ -13,9 +16,73 @@ public class TrabajoServiceImpl implements ITrabajoService {
 	@Autowired
 	private ITrabajoDAO trabajoDao;
 	
+	@Autowired
+	private ITrabajadorDAO trabajadorDao;
+	
 	@Override
 	@Transactional(readOnly=true)
 	public List<Trabajo> findAll(){
 		return (List<Trabajo>) trabajoDao.findAll();
 	}
+	
+	@Override
+    @Transactional
+    public Trabajo save(Trabajo trabajo) {
+        return trabajoDao.save(trabajo);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(String id) {
+        trabajoDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Trabajo findById(String id) {
+        return trabajoDao.findById(id).orElse(null);
+    }
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Trabajo> findPendientesTrabajador(String idTrabajador, String contraseña) {
+        return trabajoDao.findPendientesTrabajador(idTrabajador, contraseña);
+    }
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Trabajo> findFinalizadosTrabajador(String idTrabajador, String contraseña) {
+        return trabajoDao.findFinalizadosTrabajador(idTrabajador, contraseña);
+    }
+    
+    @Override
+    @Transactional
+    public Trabajo finalizarTrabajo(String id) {
+        Trabajo trabajo = trabajoDao.findById(id).orElse(null);
+        if (trabajo != null) {
+            trabajo.setFechaFin(new Date());
+            return trabajoDao.save(trabajo);
+        }
+        return null;
+    }
+    
+    @Override
+    @Transactional
+    public int asignarTrabajo(String idTrabajo, String idTrabajador) {
+    	Trabajo trabajo = trabajoDao.findById(idTrabajo).orElse(null);
+        Trabajador trabajador = trabajadorDao.findById(idTrabajador).orElse(null);
+
+        if (trabajo == null || trabajador == null) {
+            return -1;
+        }
+
+        if (!trabajo.getCategoria().equals(trabajador.getEspecialidad())) {
+            return -2;
+        }
+
+        trabajo.setIdTrabajador(idTrabajador);
+        trabajoDao.save(trabajo);
+        
+        return 1;
+    }
 }
